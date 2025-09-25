e68c205a15cbf667f5573e2ff99a9a092a80fd71
DESKTOP-GR883KV\user <ryukyungwoo1220@gmail.com>
2025-09-25 12:15:00 +0900

    :tada: init: spring cloud api gateway 초기 설정

---
+++ src/main/java/hello/pet/springapigatewayservice/SpringApiGatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package hello.pet.springapigatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApiGatewayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringApiGatewayServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hello/pet/springapigatewayservice/SpringApiGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.pet.springapigatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApiGatewayServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
